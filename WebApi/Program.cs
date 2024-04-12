ba898d20ca1f0af1ba7d80a32eaf955fcad1e46a
Simón Irrazábal <simon_irra@hotmail.com>
2024-04-12 06:49:59 -0400

    Boton de autorizacion swagger

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -7,37 +7,33 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-//builder.Services.AddSwaggerGen(c =>
-//    {
-//        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Web Api", Version = "v1" });
-//        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-//        {
-//            Description = "JWT Authorization header using the Bearer scheme",
-//            Name = "Authorization",
-//            In = ParameterLocation.Header,
-//            Type = SecuritySchemeType.Http,
-//            BearerFormat = "JWT",
-//            Scheme = "bearer"
-//        });
-
-//        c.AddSecurityRequirement(new OpenApiSecurityRequirement
-//        {
-//            {
-//                new OpenApiSecurityScheme
-//                {
-//                    Reference = new OpenApiReference
-//                    {
-//                        Type = ReferenceType.SecurityScheme,
-//                        Id = "Bearer"
-//                    }
-//                },
-//                new string[] { }
-//            }
-//        });
-//    });
-
-
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddWebApi(builder.Configuration);
@@ -48,40 +44,6 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    //app.UseSwaggerUI(c =>
-    //{
-    //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web Api V1");
-    //    c.RoutePrefix = string.Empty;
-    //});
-    //app.UseSwaggerUI();
-    //builder.Services.AddSwaggerGen(c =>
-    //{
-    //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Web Api", Version = "v1" });
-    //    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    //    {
-    //        Description = "JWT Authorization header using the Bearer scheme",
-    //        Name = "Authorization",
-    //        In = ParameterLocation.Header,
-    //        Type = SecuritySchemeType.Http,
-    //        BearerFormat = "JWT",
-    //        Scheme = "bearer"
-    //    });
-
-    //    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    //    {
-    //        {
-    //            new OpenApiSecurityScheme
-    //            {
-    //                Reference = new OpenApiReference
-    //                {
-    //                    Type = ReferenceType.SecurityScheme,
-    //                    Id = "Bearer"
-    //                }
-    //            },
-    //            new string[] { }
-    //        }
-    //    });
-    //});
 }
 
 app.UseMiddleware<ExceptionHandleMiddleware>();
